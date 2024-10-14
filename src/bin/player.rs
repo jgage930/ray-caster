04285adb5f4d2b0aa266c6b5eea47c1741177797
Jack Gage <jgage930@gmail.com>
2024-10-14 11:38:43 -0500

    bin for player

---
+++ src/bin/player.rs
@@ -0,0 +1,28 @@
+use ray_caster::map::*;
+use ray_caster::player::*;
+use ray_caster::*;
+
+fn main() {
+    let map_file = "
+        1111111111
+        1        1
+        1  1111111
+        1        1
+        1    11111
+        1    1   1
+        1   1    1
+        1        1
+        1        1
+        1111111111
+    "
+    .trim();
+
+    let map = Map::new(64, map_file);
+
+    let mut buf = map.into_buffer();
+
+    buf.set_draw_color(Color::GREEN);
+    buf.draw(map);
+
+    save_ppm("images/player.ppm", &buf).expect("Could not write to file.");
+}
