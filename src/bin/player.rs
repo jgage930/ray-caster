cd3787833f8c6655937186c773a2872feab052ee
Jack Gage <jgage930@gmail.com>
2024-10-14 11:39:42 -0500

    bin for player

--- src/bin/player.rs
+++ src/bin/player.rs
@@ -18,11 +18,15 @@ fn main() {
     .trim();
 
     let map = Map::new(64, map_file);
-
     let mut buf = map.into_buffer();
 
     buf.set_draw_color(Color::GREEN);
     buf.draw(map);
+
+    let player = Player::new(100, 100);
+
+    buf.set_draw_color(Color::RED);
+    buf.draw(player);
 
     save_ppm("images/player.ppm", &buf).expect("Could not write to file.");
 }
