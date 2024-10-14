bd5ccbc45694a8ab2751b89a8ff917de186f118c
Jack Gage <jgage930@gmail.com>
2024-10-14 11:29:04 -0500

    draw map fully

--- src/bin/map.rs
+++ src/bin/map.rs
@@ -19,12 +19,7 @@ fn main() {
 
     let mut buf = map.into_buffer();
 
-    // let mut buf = FrameBuffer::new(500, 500, &Color::BLACK);
     buf.set_draw_color(Color::GREEN);
-
-    // let rect = Rect::new(100, 150, 200, 50);
-    // buf.draw_rect(&rect);
-
     buf.draw(map);
 
     save_ppm("images/map.ppm", &buf).expect("Could not write to file.");
