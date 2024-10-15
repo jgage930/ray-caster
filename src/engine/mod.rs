fe4b596dc462b953f150c27c570ac3d1d9d9a665
Jack Gage <jgage930@gmail.com>
2024-10-15 10:53:07 -0500

    trait

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,5 +1,12 @@
 pub mod map;
 pub mod renderer;
 
+use renderer::Renderer;
+
 /// An alias for the result type used by sdl2.
 pub type SdlResult<T> = std::result::Result<T, String>;
+
+// A trait for anything that can be drawn my the renderer.
+pub trait Drawable {
+    fn draw(&self, renderer: &mut Renderer);
+}
